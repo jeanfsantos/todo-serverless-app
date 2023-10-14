a1fb1482000998aea6b5669b3a22b0e525d5f757
Jean Santos <jeanwfsantos@gmail.com>
2023-10-14 15:40:39 +0100

    add auth0autorizer and disable incomplete features

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -12,7 +12,8 @@ const logger = createLogger('auth')
 // TODO: Provide a URL that can be used to download a certificate that can be used
 // to verify JWT token signature.
 // To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
-const jwksUrl = '...'
+const jwksUrl =
+  'https://dev-fqiz0hf1no3st0ac.us.auth0.com/.well-known/jwks.json'
 
 export const handler = async (
   event: CustomAuthorizerEvent
@@ -61,7 +62,10 @@ async function verifyToken(authHeader: string): Promise<JwtPayload> {
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
   // You can read more about how to do this here: https://auth0.com/blog/navigating-rs256-and-jwks/
-  return undefined
+  const jwks = await getJwks(jwksUrl)
+  const key = await getSigningKey(jwks, jwt.header.kid)
+
+  return verify(token, key.publicKey, { algorithms: ['RS256'] }) as JwtPayload
 }
 
 function getToken(authHeader: string): string {
@@ -74,4 +78,46 @@ function getToken(authHeader: string): string {
   const token = split[1]
 
   return token
+}
+
+async function getJwks(jwksUrl: string): Promise<any> {
+  const res = await Axios.get(jwksUrl, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': '"true"'
+    }
+  })
+
+  return res.data.keys
+}
+
+async function getSigningKey(jwks: any, kid: string): Promise<any> {
+  const signingKeys = jwks
+    .filter(
+      (key) =>
+        key.use === 'sig' && // JWK property `use` determines the JWK is for signing
+        key.kty === 'RSA' && // We are only supporting RSA
+        key.kid && // The `kid` must be present to be useful for later
+        key.x5c &&
+        key.x5c.length // Has useful public keys (we aren't using n or e)
+    )
+    .map((key) => {
+      return { kid: key.kid, nbf: key.nbf, publicKey: certToPEM(key.x5c[0]) }
+    })
+
+  const signingKey = signingKeys.find((key) => key.kid === kid)
+  if (!signingKey) {
+    logger.error('Invalid signing keys')
+    throw new Error('Invalid signing keys')
+  }
+
+  logger.info('Signing keys created successfully', signingKey)
+  return signingKey
+}
+
+function certToPEM(cert: string): string {
+  cert = cert.match(/.{1,64}/g).join('\n')
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`
+  return cert
 }
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -4,16 +4,44 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors } from 'middy/middlewares'
 
-import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
-import { getUserId } from '../utils';
+// import { getTodosForUser as getTodosForUser } from '../../businessLogic/todos'
+import { getUserId } from '../utils'
+
+import { createLogger } from '../../utils/logger'
+
+const logger = createLogger('getTodos')
 
 // TODO: Get all TODO items for a current user
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     // Write your code here
-    const todos = '...'
+    logger.info('--- GET TODOS ---')
 
-    return undefined
+    try {
+      const userId = getUserId(event)
+
+      logger.info('--- userId ---', userId)
+
+      const todos = []
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          items: todos
+        })
+      }
+    } catch (e) {
+      logger.error('--- GET TODOS FAILED ---', e.message, e)
+
+      return {
+        statusCode: 500,
+        body: JSON.stringify({
+          message: 'Unexpected Error'
+        })
+      }
+    }
+  }
+)
 
 handler.use(
   cors({
